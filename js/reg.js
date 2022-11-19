19af2afc72ce968fc0b9f74d52fa7ed7da1af893
Chittesh S <110821506+Chittesh14@users.noreply.github.com>
2022-11-19 10:49:51 +0530

    Update reg.js

---
+++ js/reg.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#save").click(function(){
+        $.ajax({
+            url:"insert.php",
+            type:"post",
+            data:$("#frm").serialize(),
+            success:function(d)
+            {
+                alert(d);
+            }
+        });
+    });
+});
